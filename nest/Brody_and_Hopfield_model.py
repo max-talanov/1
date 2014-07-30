46612cc29fcaaf9089aae240ee86c9754a289879
Max Talanov <max.talanov@gmail.com>
2014-07-30 08:33:20 +0400

    added time of calculation

--- nest/Brody_and_Hopfield_model.py
+++ nest/Brody_and_Hopfield_model.py
@@ -27,7 +27,7 @@ import numpy
 N=1000 #Jellyfish
 
 N=1000000 #Cockroach 
-N=4000000 #Mouse cortex
+N=4000000 #Mouse cortex = 01:02:27
 
 bias_begin=140. # bias current from...
 bias_end=200.   # ...to (ms) 
