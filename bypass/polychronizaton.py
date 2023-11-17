d4951b33ab59c8cd191907b8beb5b57fb51cc52d
Max Talanov <max.talanov@gmail.com>
2023-11-17 23:08:19 +0100

    started polychronisation encoding

--- bypass/polychronizaton.py
+++ bypass/polychronizaton.py
@@ -1,4 +1,27 @@
 import pickle
+from struct import *
 import logging
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+logging.info('started ...')
+file_name = 'bypass/Izhi.py'
+size = 10 # 1kB = 2^10B
 
-print('started ...')
+with open(file_name, "r") as f:
+    counter = 0
+    while (byte := f.read(1)):
+        # convert address and value to binary 
+        address_bin = format(counter, 'b')
+        value_bin = format(ord(byte), '08b') ## pack(">c", byte)
+
+        logging.info(byte)
+        logging.info(address_bin)
+        logging.info(value_bin)
+        
+        # get the bin address 
+        # get bin value 
+        # encode address->value
+        # encode crossections 
+
+        counter += 1
+
+logging.info('ended ...')
