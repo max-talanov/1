28fe5de895f0e830cb294f6ca86ee8bfffff41f7
max-talanov <max.talanov@gmail.com>
2025-03-11 20:15:09 +0100

    almost fixed wmax - w

--- bypass/up_down_scale.py
+++ bypass/up_down_scale.py
@@ -4,11 +4,12 @@ from mpl_toolkits.mplot3d import Axes3D
 
 def weight_change_function(x, y):
     x=x*4
+    y=y*-1-0.6
     gaussian_decay = np.exp(-0.5 * (x**2 + y**2))
     return gaussian_decay * np.sin(x) * np.cos(y)
 
 x = np.linspace(-1, 1, 100)
-y = np.linspace(-1.6, -0.5, 100)
+y = np.linspace(-0.0, 1.0, 100)
 
 x, y = np.meshgrid(x, y)
 z = weight_change_function(x, y)
@@ -21,6 +22,6 @@ ax.plot_surface(x, y, z, cmap='viridis', alpha=0.8)
 ax.set_xlabel('Ca (rel)')
 ax.set_ylabel('w_max - w')
 ax.set_zlabel('Δw')
-ax.set_title('3D STDP')
+ax.set_title('3D CaLDP')
 
 plt.show()
