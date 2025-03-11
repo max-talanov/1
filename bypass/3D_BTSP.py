bc024f9549fdb069c204fef5db9db5dade3cc452
max-talanov <max.talanov@gmail.com>
2025-03-11 20:09:15 +0100

    added 3D BTSP

---
+++ bypass/3D_BTSP.py
@@ -0,0 +1,44 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+def gaussian(x, mean, std_dev):
+    """
+    Compute the Gaussian (normal) distribution function.
+    :param x: Input value or array.
+    :param mean: Mean of the distribution.
+    :param std_dev: Standard deviation of the distribution.
+    :return: Gaussian function value.
+    """
+    coefficient = 1 / (std_dev * np.sqrt(2 * np.pi))
+    exponent = np.exp(-0.5 * ((x - mean) / std_dev) ** 2)
+    return coefficient * exponent - 0.1
+
+def decay_function(z):
+    """
+    Compute decay that converges to 0 along the x-axis.
+    :param z: Input value or array.
+    :return: Decay function value.
+    """
+    return np.exp(-z)
+
+# Define parameters
+mean = 0
+std_dev = 1
+x = np.linspace(-5, 5, 100)
+z = np.linspace(0, 5, 100)
+X, Z = np.meshgrid(x, z)
+Y = gaussian(X, mean, std_dev) * decay_function(Z)
+Y = (Y - np.min(Y)) / (np.max(Y) - np.min(Y)) * (2.0 - (-0.4)) + (-0.4)
+
+# Plot the 3D Gaussian distribution with decay
+fig = plt.figure(figsize=(10, 7))
+ax = fig.add_subplot(111, projection='3d')
+ax.plot_surface(X, Z, Y, cmap='viridis')
+ax.set_xlabel('Time (s)')
+ax.set_ylabel('w_max - w')
+ax.set_zlabel('Δw')
+ax.set_zlim(-0.4, 2)
+
+ax.set_title('3D BTSP')
+plt.show()
