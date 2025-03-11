cd2791126809f4261a636b51b2ae1433bb5a4f28
max-talanov <max.talanov@gmail.com>
2025-03-11 20:17:22 +0100

    fixed wmax - w

--- bypass/up_down_scale.py
+++ bypass/up_down_scale.py
@@ -4,7 +4,7 @@ from mpl_toolkits.mplot3d import Axes3D
 
 def weight_change_function(x, y):
     x=x*4
-    y=y*-1-0.6
+    y=(y-0.6)-1
     gaussian_decay = np.exp(-0.5 * (x**2 + y**2))
     return gaussian_decay * np.sin(x) * np.cos(y)
 
