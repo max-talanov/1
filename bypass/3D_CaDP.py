d243295c2afc7c4e548999ac929597406d790b85
max-talanov <max.talanov@gmail.com>
2025-03-12 21:21:16 +0100

    fixed z scale of BPSP

--- bypass/3D_CaDP.py
+++ bypass/3D_CaDP.py
@@ -19,9 +19,9 @@ ax = fig.add_subplot(111, projection='3d')
 
 ax.plot_surface(x, y, z, cmap='viridis', alpha=0.8)
 
-ax.set_xlabel('Ca (rel)')
+ax.set_xlabel('CaMKII (rel)')
 ax.set_ylabel('w_max - w')
 ax.set_zlabel('Δw')
-ax.set_title('3D CaLDP')
+ax.set_title('3D CaMKIILDP')
 
 plt.show()
