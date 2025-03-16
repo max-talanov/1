a49848b7b5ff0ccb13acb48314ace904d93707da
Kipelkin <139872886+Kipelkin@users.noreply.github.com>
2025-03-16 20:25:25 +0300

    Update 3D_CaDP.py

--- bypass/3D_CaDP.py
+++ bypass/3D_CaDP.py
@@ -1,23 +1,35 @@
+import matplotlib
+matplotlib.use('TkAgg')
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 def weight_change_function(x, y):
-    x=x*4
-    y=(y-0.6)-1
+    y = -(y - 0.6) - 1
     gaussian_decay = np.exp(-0.5 * (x**2 + y**2))
     return gaussian_decay * np.sin(x) * np.cos(y)
 
-x = np.linspace(-1, 1, 100)
-y = np.linspace(-0.0, 1.0, 100)
+x = np.linspace(-5, 5, 100)
+y = np.linspace(0.0, 1.0, 100)
 
 x, y = np.meshgrid(x, y)
-z = 3 * weight_change_function(x, y)
+
+z = 1.2 * weight_change_function(x, y)
+z = np.where(z < 0, z * 1.686, z * 0.2)
+
+z_min = z.min()
+z_max = z.max()
+
+print(f"Min Z: {z_min}")
+print(f"Max Z: {z_max}")
 
 fig = plt.figure(figsize=(8, 6))
 ax = fig.add_subplot(111, projection='3d')
 
 ax.plot_surface(x, y, z, cmap='viridis', alpha=0.8)
+
+ax.set_zlim(-1.2 , 0.3 )
+
 
 ax.set_xlabel('T h')
 ax.set_ylabel('w_max - w')
